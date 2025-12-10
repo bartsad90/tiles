7d3be73ec97dd55bba63a96f985bdfc57871a924
bartsad <bartsad@gmail.com>
2025-12-10 11:56:40 +0100

    Adds negative point indicators

--- scripts/negPointsBar.js
+++ scripts/negPointsBar.js
@@ -9,12 +9,31 @@ playerData[activePlayer][1].negPointTiles.forEach((tile) => {
     player${activePlayer}-neg-point-tile 
     player${activePlayer}-tile-${tile.tileColor}
     ">
+      <div class="neg-point-indicator neg-point-indicator-${playerData[activePlayer][1].negPointTiles.indexOf(tile)}"></div>
     <img class="tile-image" src="../images/tile-${tile.tileColor}.png"></div>
     </div>`;
   })
   document.querySelector(`.js-player${activePlayer}-neg-point-container`).innerHTML = innerHTML;
   console.log('negPointTiles: ', playerData[activePlayer][1].negPointTiles, 'activePlayer: ', activePlayer);
-  
+
+  renderNegPointTileIndicators();
+}
+
+function renderNegPointTileIndicators (){
+  document.querySelectorAll('.neg-point-indicator').forEach((element) => {
+    if (element.classList.contains('neg-point-indicator-0') || element.classList.contains('neg-point-indicator-1')) {
+      element.innerHTML = -1;
+    }
+    else if (element.classList.contains('neg-point-indicator-2') 
+      || element.classList.contains('neg-point-indicator-3') 
+      || element.classList.contains('neg-point-indicator-4')) {
+      element.innerHTML = -2;
+    }
+    else if (element.classList.contains('neg-point-indicator-5') 
+      || element.classList.contains('neg-point-indicator-6')) {
+      element.innerHTML = -3;
+    }
+  })
 }
 
 export function generateBlankNegPointTiles(negPointTiles) {
